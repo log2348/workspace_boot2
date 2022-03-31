0b369e9b8a565c1e7449de8e44c348a37a0dd1d7
honggildong <jaemin4192@gmail.com>
2022-03-31 16:15:15 +0900

    로그

---
+++ chapter03/src/ch12/MyLog.java
@@ -0,0 +1,33 @@
+package ch12;
+
+import java.util.logging.Logger;
+
+class Student {
+	String name;
+	int grade;
+	
+}
+
+public class MyLog {
+	
+	public static void main(String[] args) {
+		// 로그 남겨 보기
+		Logger logger = Logger.getLogger("MyLog");
+		
+		Student student = null;
+		
+		try {
+			student.name = "홍길동";
+			
+		} catch (NullPointerException e) {
+			e.printStackTrace();
+			System.out.println("--------------------------------------------------");
+			logger.warning("오류 확인: " + e.toString());
+		}
+		
+		System.out.println("여기 코드 실행 확인");
+		
+	}
+	
+
+}
