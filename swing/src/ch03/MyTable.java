e30622f17c8cb5fa1edd62562c7dddaac097fc7e
honggildong <jaemin4192@gmail.com>
2022-04-04 15:01:13 +0900

    JTable

--- swing/src/ch03/MyTable.java
+++ swing/src/ch03/MyTable.java
@@ -5,27 +5,47 @@ import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.JTextField;
 
-public class MyTable extends JFrame{
-	
-	private JTable table = new JTable(5, 3);
-	
-	private final String[] titles = { "이름", "전화번호", "비고"};
-	private JTextField[] contents = new JTextField[5];
-	
+public class MyTable extends JFrame {
+
+	private JTable table;
+	private JFrame frame;
+
+	private final String[] titles = { "이름", "전화번호", "비고" };
+	private String[][] contents = { { "박지현1", "010-0000-0000" }, { "박지현2", "010-0000-0000" },
+			{ "박지현3", "010-0000-0000" } };
+
 	private JScrollPane jScrollPane = new JScrollPane();
 
 	public MyTable() {
+		initData();
+		setInitLayout();
+
+	}
+
+	private void initData() {
+		table = new JTable(contents, titles);
+		frame = new JFrame();
+
+		setTitle("JTable 연습");
+		setSize(500, 200);
+		
+		table.setBounds(30, 40, 200, 300);
+		JScrollPane jsp = new JScrollPane(table);
+		
+		frame.add(jsp);
+		frame.setSize(500, 200);
+		frame.setVisible(true);
 		
 	}
-	
-	private void initData() {
-		setTitle("My Table");
-	}
-	
+
 	private void setInitLayout() {
 		setVisible(true);
 		setResizable(false);
-		
+
 	}
-	
+
+	public static void main(String[] args) {
+		new MyTable();
+	}
+
 }
