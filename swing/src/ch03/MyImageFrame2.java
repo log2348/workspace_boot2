361e076c6518f21d60739488614cc7e1d98deb13
honggildong <jaemin4192@gmail.com>
2022-04-04 14:27:53 +0900

    add eventListener

--- swing/src/ch03/MyImageFrame2.java
+++ swing/src/ch03/MyImageFrame2.java
@@ -85,5 +85,9 @@ public class MyImageFrame2 extends JFrame {
 
 		}
 	} // end of inner class
+	
+	public void addEventListener() {
+		System.out.println("브랜치를 만들었습니다.");
+	}
 
 } // end of outer class
